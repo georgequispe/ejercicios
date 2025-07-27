f957eafaa9608d97b1480c1c1c9d4915a5c111f0
george <fuerzayfegeorge@hotmail.com.ar>
2025-07-27 17:55:17 -0300

    modique la layout para activar el boton onstarat

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import Welcome from "./Welcome";
+import Workout from "./Workout";
 import { LayoutWrapper, MainContent } from "../styled";
-const Layout = ({ children }) => {
+
+const Layout = () => {
+  const [currentScreen, setCurrentScreen] = useState("welcome");
+
+  const handleStart = () => setCurrentScreen("workout");
+
   return (
     <LayoutWrapper>
       <Header />
-      <MainContent>{children}</MainContent>
+      <MainContent>
+        {currentScreen === "welcome" && <Welcome onStart={handleStart} />}
+        {currentScreen === "workout" && <Workout />}
+      </MainContent>
       <Footer />
     </LayoutWrapper>
   );
